a032b11e00d1822c36c3885da6345d6fe40a6061
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2022-02-16 11:42:06 +0530

    Couch db

---
+++ src/main/java/com/javatechie/couchbase/api/model/Customer.java
@@ -0,0 +1,27 @@
+package com.javatechie.couchbase.api.model;
+
+import org.springframework.data.couchbase.core.mapping.Document;
+
+import com.couchbase.client.java.repository.annotation.Field;
+import com.couchbase.client.java.repository.annotation.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.couchbase.core.mapping.id.GeneratedValue;
+import org.springframework.data.couchbase.core.mapping.id.GenerationStrategy;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationStrategy.UNIQUE)
+	private int id;
+	@Field
+	private String name;
+	@Field
+	private String[] address;
+
+}
