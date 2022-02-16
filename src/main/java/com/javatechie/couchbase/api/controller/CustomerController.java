3f3fd2630064e48df588df29a8d41976d78f6aae
Sambit Kumar Pradhan <48365115+psambitkumar@users.noreply.github.com>
2022-02-16 12:57:58 +0530

    Controller Added

--- src/main/java/com/javatechie/couchbase/api/controller/CustomerController.java
+++ src/main/java/com/javatechie/couchbase/api/controller/CustomerController.java
@@ -4,9 +4,11 @@ import com.javatechie.couchbase.api.model.Customer;
 import com.javatechie.couchbase.api.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -18,16 +20,32 @@ public class CustomerController {
 
     @PostConstruct
     public void saveCustomers() {
-        repository.saveAll(Stream
-                .of(new Customer(1, "Dilip", new String[] { "Odisha", "Koraput" }),
-                        new Customer(2, "Nitish", new String[] { "Odisha", "Bhadrak" }),
-                        new Customer(3,"Sambit",new String[] {"Odisha", "Salipur"}),
-                        new Customer(4,"Hrusi",new String[] {"Odisha", "Balesore"}))
-                .collect(Collectors.toList()));
+        try {
+            repository.saveAll(Stream
+                    .of(new Customer(01, "Dilip", new String[] { "Odisha", "Koraput" }),
+                            new Customer(02, "Nitish", new String[] { "Odisha", "Bhadrak" }),
+                            new Customer(03,"Sambit",new String[] {"Odisha", "Salipur"}),
+                            new Customer(04,"Hrusi",new String[] {"Odisha", "Balesore"}))
+                    .collect(Collectors.toList()));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @GetMapping("/ViewAllCustomers")
     public Iterable<Customer> viewCustomers() {
         return repository.findAll();
+    }
+
+    @GetMapping("/Delete{id}")
+    public Iterable<Customer> deleteById(@PathVariable("id") int id) {
+        Iterable<Customer> iterable = null;
+        try {
+            repository.deleteById(id);
+            iterable = repository.findAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return iterable;
     }
 }
